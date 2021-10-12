415ff098171b6dc0baee6328b28fc5a52b868412
Venkatesh Bheemagani <venkatesh.bheemagani@gmail.com>
2021-10-12 14:47:05 -0700

    ConditionalRendering

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const app = Vue.createApp({
 data(){
     return{
+        showBooks: true,
         title: 'The Final Empire',
         author: 'Brandon Sanderson',
         age: 45
     }
 },
 methods: {
-    changeTitle(title){
-        //this.title = 'Word of Radinance'
-        this.title=title
+    toggleShowBooks(){
+        this.showBooks = !this.showBooks
     }
 }
 })
